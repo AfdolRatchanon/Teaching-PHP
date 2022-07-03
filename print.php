417eac5a6d299a3eee4d9eed5d1c72bd96616a5a
AfdolRatchanon <78583656+AfdolRatchanon@users.noreply.github.com>
2022-07-03 22:07:28 +0700

    update

--- print.php
+++ print.php
@@ -2,7 +2,7 @@
      print "<h2>PHP is Fun!</h2>";
      print "Hello world!<br>";
      print "I'm about to learn PHP!";
-     print "This ", "string ", 
-           "was ", "made ", 
-           "with multiple parameters.";
+//      print "This ", "string ", 
+//            "was ", "made ", 
+//            "with multiple parameters.";
 ?>
