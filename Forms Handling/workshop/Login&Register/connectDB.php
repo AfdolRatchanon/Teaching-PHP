c692f1918219ea5e632efd94f71e8a3faf2465f8
AFDOL <78583656+AfdolRatchanon@users.noreply.github.com>
2022-08-07 13:04:10 +0700

    insert into (MySQL)

--- Forms Handling/workshop/Login&Register/connectDB.php
+++ Forms Handling/workshop/Login&Register/connectDB.php
@@ -2,7 +2,7 @@
 // error_reporting(E_ALL);
 
 //เชื่อมต่อฐานข้อมูล
-$conn = mysqli_connect("localhost", "root", "", "student");
+$conn = mysqli_connect("localhost", "root", "", "student", 3306);
 
 //ตรวจสอบการเชื่อมต่อฐานข้อมูล
 if (!$conn) {
